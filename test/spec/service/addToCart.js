6dd83bcc3f636e61c22d8ad159d9e1a16e2e71e9
zzh <xiaoyanzhuzzh@sina.com>
2014-09-16 19:47:43 +0800

    make the test coverage of addToCart is 100%

--- test/spec/service/addToCart.js
+++ test/spec/service/addToCart.js
@@ -13,10 +13,20 @@ describe('addToCartService', function () {
         });
     });
 
-    it ('should have isExistInCart function and return undefined', function(){
+    it ('should have isExistInCart function and return undefined because category.length is 0', function(){
 
       var barcode = 'ITEM000000';
       var cartItems = [];
+      var result = addToCartService.isExistInCart(barcode, cartItems);
+
+      expect(result).toBe(undefined);
+    });
+
+    it ('should have isExistInCart function and return undefined because the barcodes id not equal', function(){
+
+      var barcode = 'ITEM000000';
+      var item = {barcode:'ITEM000001', name: '雪碧', unit:'瓶', price:3.00, category:'饮品'};
+      var cartItems = [{item: item, number: 1}];
       var result = addToCartService.isExistInCart(barcode, cartItems);
 
       expect(result).toBe(undefined);

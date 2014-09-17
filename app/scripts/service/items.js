063933eaeab0a92ddb0889fe6d72d2bcb50661e7
zzh <xiaoyanzhuzzh@sina.com>
2014-09-17 14:15:41 +0800

    add localStorageService to itemsService

--- app/scripts/service/items.js
+++ app/scripts/service/items.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('letusgoApp')
-    .service('ItemsService',function(){
+    .service('ItemsService',function(localStorageService){
 
         this.getItems = function(){
           var items = [
@@ -13,7 +13,17 @@ angular.module('letusgoApp')
               {barcode:'ITEM000005', name:'方便面', unit:'袋',price: 4.50, category:'零食'},
            ];
 
-          Util.localStorage.setStorageItem('items',items);
+          localStorageService.set('items',items);
           return items;
+         };
+
+         this.get = function(key){
+
+           return localStorageService.get(key);
+         };
+
+         this.set = function (key, value){
+           
+           localStorageService.set(key, value);
          };
     });

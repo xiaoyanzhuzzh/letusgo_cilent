9bd8df2701d12ab1a0944eee24be7b8bc9b762af
zzh <xiaoyanzhuzzh@sina.com>
2014-09-18 22:53:47 +0800

    index view add highlight

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -4,6 +4,8 @@
 angular.module('letusgoApp')
   .controller('HomeCtrl', function ($scope, ItemsService, CategoryService) {
 
+    $scope.$emit('to-parent-homeActive');
+
     $scope.items = ItemsService.getItems();
     $scope.categorys = CategoryService.getCategorysAndId($scope.items);
   });

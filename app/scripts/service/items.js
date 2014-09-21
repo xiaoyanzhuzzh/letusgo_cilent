882c8775c0c7dd7949c0bd3f9f853c2cace68ac9
zzh <xiaoyanzhuzzh@sina.com>
2014-09-22 01:47:18 +0800

    update function

--- app/scripts/service/items.js
+++ app/scripts/service/items.js
@@ -1,85 +1,57 @@
 'use strict';
 
 angular.module('letusgoApp')
-    .service('ItemsService',function(localStorageService, $http){
+  .service('ItemsService',function(localStorageService, $http){
 
-        function getItemsData(callback){
+    function getItemsData(callback) {
 
-          $http.get('/api/items')
-           .success(function (data) {
-             callback(data);
-           });
-        }
+      $http.get('/api/items')
+       .success(function(data) {
+          callback(data);
+       });
+    }
 
-        function setItemsData(items, callback){
+//    function postItemsData(items, callback) {
+//
+//      $http({method: 'POST', url: '/api/items', data:{'items': items}})
+//       .success(function(data) {
+//
+//         callback(data);
+//       });
+//    }
 
-          $http({method: 'POST', url: '/api/items', data:{'items': items}})
-           .success(function (data) {
+//    function deleteItemData(id) {
+//
+//      $http.delete('/api/items/' + id);
+//    }
+//
+//    function putItemData(item) {
+//      $http({method: 'PUT', url: '/api/items/' + item.id, data:{'item': item}});
+//
+//    }
+    this.getItems = function(callback){
 
-             callback(data);
-           });
-        }
+      getItemsData(function(data){
+        callback(data);
+      });
+    };
+//
+//    this.deleteItem = function(id){
+//
+//      deleteItemData(id);
+//    };
+//
+//    this.putItem = function(item) {
+//      putItemData(item);
+//    };
 
-        this.getItems = function(callback){
+    this.get = function(key){
 
-          getItemsData(function(data){
+     return localStorageService.get(key);
+    };
 
-            callback(data);
-          });
-        };
+    this.set = function (key, value){
 
-        this.deleteItem = function (item, callback){
-
-          getItemsData(function(data){
-            var items = data;
-            var index = _.findIndex(items, {'id': item.id});
-            items.splice(index, 1);
-
-            setItemsData(items, function(newData){
-              if(newData === 'OK'){
-                callback(items);
-              }
-            });
-          });
-        };
-
-        this.modifyItem = function (newItem, callback){
-
-          getItemsData(function(data){
-            var items = data;
-            var index = _.findIndex(items, {'name': newItem.name});
-            items[index] = newItem;
-            setItemsData(items, function(newData){
-              if(newData === 'OK'){
-                callback(items);
-              }
-            });
-          });
-        };
-        // this.modifyItem = function (newItem, items) {
-        //
-        //   var changingItem = localStorageService.get('changingItem');
-        //
-        //   for (var i = 0; i < items.length; i++) {
-        //
-        //     if(changingItem.name === items[i].name) {
-        //
-        //       items[i].name = newItem.name;
-        //       items[i].unit = newItem.unit;
-        //       items[i].price = newItem.price;
-        //     }
-        //   }
-        //   localStorageService.set('items', items);
-        //   return items;
-        // };
-
-         this.get = function(key){
-
-           return localStorageService.get(key);
-         };
-
-         this.set = function (key, value){
-
-           localStorageService.set(key, value);
-         };
-    });
+     localStorageService.set(key, value);
+    };
+  });

628f7366c2010a82a2cbc496f9c8331a43a080a9
zzh <xiaoyanzhuzzh@sina.com>
2014-09-22 19:46:07 +0800

    modify function come true

--- app/scripts/service/category.js
+++ app/scripts/service/category.js
@@ -12,15 +12,15 @@ angular.module('letusgoApp')
        });
     }
 
-    function setCategoriesData(categories, callback){
+    function deleteCategoryData(id) {
 
-      $http({method: 'POST', url: '/api/categories', data: {'categories': categories}})
-       .success(function(data){
-
-         callback(data);
-       });
+      $http.delete('/api/categories/' + id);
     }
 
+    function putCategoryData(category) {
+
+      $http({method: 'PUT', url: '/api/categories/' + category.id, data:{'category': category}});
+    }
 
     this.getCategories = function (callback) {
 
@@ -30,87 +30,55 @@ angular.module('letusgoApp')
       });
     };
 
-        this.getCategorys = function (items) {
+    this.deleteCategory = function(id) {
 
-          var categoryNames = [];
+      deleteCategoryData(id);
+    };
 
-          for (var i = 0; i < items.length; i++) {
+    this.putCategory = function(category) {
 
-            if (!_.contains(categoryNames, items[i].category)) {
+      putCategoryData(category);
+    };
+    this.deleteItem = function (category, items) {
 
-              categoryNames.push(items[i].category);
-            }
-          }
-          return categoryNames;
+      for(var i = 0; i < items.length; i++){
+
+        if(items[i].category === category.name){
+
+          items = _.without(items, items[i]);
+          i--;
+        }
+      }
+      localStorageService.set('items', items);
+      return items;
         };
 
-        this.getCategorysAndIds = function (items) {
+    this.changeCategory = function (category, categories) {
 
-          var categoryNames = this.getCategorys(items);
-          var categories = [];
+      for (var i = 0; i < categories.length; i++){
 
-          for (var i = 0; i < categoryNames.length; i++) {
+        if(category.name === categories[i].name){
 
-            categories.push({id: i, name: categoryNames[i]});
-            localStorageService.set('categories', categories);
-          }
-          return categories;
-        };
+          categories[i].name = category.name;
+          localStorageService.set('categories', categories);
+        }
+      }
+      return categories;
+    };
 
+    this.changeItem = function (category, items) {
 
-        this.deleteCategory = function (category, categories) {
+      var changingCategory = localStorageService.get('changingCategory');
 
-          for (var i = 0; i < categories.length; i++){
+      for(var i = 0; i < items.length; i++){
 
-            if(category.id === categories[i].id){
+        if(items[i].category === changingCategory.name){
 
-              categories = _.without(categories, categories[i]);
-              localStorageService.set('categories', categories);
-            }
-          }
-          return categories;
-        };
-
-        this.deleteItem = function (category, items) {
-
-          for(var i = 0; i < items.length; i++){
-
-            if(items[i].category === category.name){
-
-              items = _.without(items, items[i]);
-              i--;
-            }
-          }
-          localStorageService.set('items', items);
-          return items;
-        };
-
-        this.changeCategory = function (category, categories) {
-
-          for (var i = 0; i < categories.length; i++){
-
-            if(category.name === categories[i].name){
-
-              categories[i].name = category.name;
-              localStorageService.set('categories', categories);
-            }
-          }
-          return categories;
-        };
-
-        this.changeItem = function (category, items) {
-
-          var changingCategory = localStorageService.get('changingCategory');
-
-          for(var i = 0; i < items.length; i++){
-
-            if(items[i].category === changingCategory.name){
-
-              items[i].category = category.name;
-              localStorageService.set('changingCategory', category);
-            }
-          }
-          localStorageService.set('items', items);
-          return items;
-        };
+          items[i].category = category.name;
+          localStorageService.set('changingCategory', category);
+        }
+      }
+      localStorageService.set('items', items);
+      return items;
+    };
   });

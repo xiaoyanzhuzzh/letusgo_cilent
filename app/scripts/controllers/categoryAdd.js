4911fbd1095730f8adadc9634b6d5fa5575a3656
zzh <xiaoyanzhuzzh@sina.com>
2014-09-22 21:37:40 +0800

    complete add function

--- app/scripts/controllers/categoryAdd.js
+++ app/scripts/controllers/categoryAdd.js
@@ -2,45 +2,44 @@
 
 
 angular.module('letusgoApp')
-    .controller('CategoryAddCtrl', function ($scope, ItemsService, CategoryService) {
+  .controller('CategoryAddCtrl', function ($scope, ItemsService, CategoryService) {
 
-        $scope.items = [];
-        ItemsService.getItems(function(data) {
-          $scope.items = data;
-        });
-
-        $scope.categories = [];
-        CategoryService.getCategories(function(data) {
-          $scope.categories = data;
-        });
-
-
-        $scope.showSignal = false;
-
-        $scope.addButton = function () {
-
-            $scope.showSignal = true;
-
-        };
-        $scope.cancelButton = function () {
-
-          $scope.showSignal = false;
-        };
-
-        $scope.deleteCurrentCategory = function (category) {
-
-            $scope.categorys = CategoryService.deleteCategory($scope.categorys, category);
-            $scope.items = CategoryService.deleteItem($scope.items, category);
-        };
-
-        $scope.addNewCategory = function (newCategory) {
-
-            var category = {id: 0, name: newCategory};
-            category.id = $scope.categorys[$scope.categorys.length - 1].id + 1;
-
-            $scope.categorys.push(category);
-            ItemsService.set('categorys', $scope.categorys);
-
-            $scope.showSignal = false;
-        };
+    $scope.items = [];
+    ItemsService.getItems(function(data) {
+      $scope.items = data;
     });
+
+    $scope.categories = [];
+    CategoryService.getCategories(function(data) {
+      $scope.categories = data;
+    });
+    
+    $scope.showSignal = false;
+
+    $scope.addButton = function () {
+
+        $scope.showSignal = true;
+
+    };
+    $scope.cancelButton = function () {
+
+      $scope.showSignal = false;
+    };
+
+    $scope.deleteCurrentCategory = function (category) {
+
+      CategoryService.deleteCategory(category.id);
+      CategoryService.getCategories(function(data) {
+        $scope.categories = data;
+      });
+    };
+
+    $scope.addNewCategory = function (newCategory) {
+
+      CategoryService.addCategory(newCategory);
+      CategoryService.getCategories(function(data) {
+        $scope.categories = data;
+      });
+        $scope.showSignal = false;
+    };
+  });

176e11daa3b136016ad385374009ab267933f1a7
zzh <xiaoyanzhuzzh@sina.com>
2014-09-22 17:20:52 +0800

    delete console.log

--- app/scripts/service/items.js
+++ app/scripts/service/items.js
@@ -7,43 +7,40 @@ angular.module('letusgoApp')
 
       $http.get('/api/items')
        .success(function(data) {
+          console.log(data);
           callback(data);
        });
     }
 
-//    function postItemsData(items, callback) {
-//
-//      $http({method: 'POST', url: '/api/items', data:{'items': items}})
-//       .success(function(data) {
-//
-//         callback(data);
-//       });
-//    }
+    function deleteItemData(id) {
 
-//    function deleteItemData(id) {
-//
-//      $http.delete('/api/items/' + id);
-//    }
-//
-//    function putItemData(item) {
-//      $http({method: 'PUT', url: '/api/items/' + item.id, data:{'item': item}});
-//
-//    }
+      $http.delete('/api/items/' + id);
+    }
+
+    function putItemData(item) {
+      $http({method: 'PUT', url: '/api/items/' + item.id, data:{'item': item}});
+
+    }
     this.getItems = function(callback){
 
-      getItemsData(function(data){
-        callback(data);
-      });
+        getItemsData(function (data) {
+
+          if(data === []) {
+
+            $http.post('/api/items');
+          }
+          callback(data);
+        });
     };
-//
-//    this.deleteItem = function(id){
-//
-//      deleteItemData(id);
-//    };
-//
-//    this.putItem = function(item) {
-//      putItemData(item);
-//    };
+
+    this.deleteItem = function(id){
+
+      deleteItemData(id);
+    };
+
+    this.putItem = function(item) {
+      putItemData(item);
+    };
 
     this.get = function(key){
 

d70460e7e1e6b6d58f88f913f3e9fa80ddc7fe96
zzh <xiaoyanzhuzzh@sina.com>
2014-09-22 19:45:26 +0800

    modify function come true

--- app/scripts/controllers/categoryModify.js
+++ app/scripts/controllers/categoryModify.js
@@ -1,41 +1,47 @@
 'use strict';
 
 angular.module('letusgoApp')
-    .controller('CategoryModifyCtrl', function ($scope, ItemsService, CategoryService) {
+  .controller('CategoryModifyCtrl', function ($scope, ItemsService, CategoryService) {
 
-        $scope.items = [];
-        ItemsService.getItems(function(data) {
-          $scope.items = data;
-        });
-
-        $scope.categories = [];
-        CategoryService.getCategories(function(data) {
-          $scope.categories = data;
-        });
-
-        $scope.modifySignal = false;
-
-        $scope.modifyCurrentCategory = function (changingCategory) {
-
-          $scope.modifySignal = true;
-          ItemsService.set('changingCategory', changingCategory);
-        };
-
-        $scope.cancelModify = function () {
-
-          $scope.modifySignal = false;
-        };
-
-        $scope.deleteCurrentCategory = function (category) {
-
-          $scope.categorys = CategoryService.deleteCategory(category, $scope.categorys);
-          $scope.items = CategoryService.deleteItem(category, $scope.items);
-        };
-
-        $scope.changeCurrentCategory = function (category) {
-
-          $scope.categorys = CategoryService.changeCategory(category, $scope.categorys);
-
-          $scope.items = CategoryService.changeItem(category, $scope.items);
-        };
+    $scope.items = [];
+    ItemsService.getItems(function(data) {
+      $scope.items = data;
     });
+
+    $scope.categories = [];
+    CategoryService.getCategories(function(data) {
+      $scope.categories = data;
+    });
+
+    $scope.modifySignal = false;
+
+    $scope.modifyButton = function (changingCategory) {
+
+      $scope.modifySignal = true;
+      $scope.categoryInfo = {
+        id: changingCategory.id,
+        name: changingCategory.name
+      };
+    };
+
+    $scope.cancelModify = function () {
+
+      $scope.modifySignal = false;
+    };
+
+    $scope.deleteCurrentCategory = function (category) {
+
+      CategoryService.deleteCategory(category.id);
+      CategoryService.getCategories(function(data) {
+        $scope.categories = data;
+      });
+    };
+
+    $scope.modifyCurrentCategory = function (newCategory) {
+      console.log(newCategory);
+      CategoryService.putCategory(newCategory);
+      CategoryService.getCategories(function(data) {
+        $scope.categories = data;
+      });
+    };
+  });

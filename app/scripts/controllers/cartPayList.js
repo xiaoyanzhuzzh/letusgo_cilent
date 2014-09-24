3251e6c5be32e1e6ce537614857c62707b2f9390
zzh <xiaoyanzhuzzh@sina.com>
2014-09-24 12:37:37 +0800

    can show payMoney view

--- app/scripts/controllers/cartPayList.js
+++ app/scripts/controllers/cartPayList.js
@@ -3,10 +3,13 @@
 angular.module('letusgoApp')
   .controller('CartPayListCtrl', function ($scope, ItemsService, CartItemsService) {
 
-     $scope.$emit('to-parent-cartPayListActive');
+    $scope.$emit('to-parent-cartPayListActive');
 
-     $scope.cartPayList = ItemsService.get('cartItems');
-     $scope.total = CartItemsService.getTotalMoney($scope.cartPayList );
-     $scope.totalNumber = CartItemsService.getTotalNumber($scope.cartPayList );
+    $scope.cartPayList = [];
+    CartItemsService.getCartItems(function(data) {
 
+      $scope.cartPayList = data;
+      $scope.total = CartItemsService.getTotalMoney($scope.cartPayList );
+      $scope.totalNumber = CartItemsService.getTotalNumber($scope.cartPayList );
+    });
   });

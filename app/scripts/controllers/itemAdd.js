198b4488d8d912a2e8f9d58b9b14265ddb7b8f1e
zzh <xiaoyanzhuzzh@sina.com>
2014-09-24 21:50:45 +0800

    refactor codes

--- app/scripts/controllers/itemAdd.js
+++ app/scripts/controllers/itemAdd.js
@@ -2,44 +2,44 @@
 
 
 angular.module('letusgoApp')
-    .controller('ItemAddCtrl', function ($scope, ItemsService, CategoryService) {
+  .controller('ItemAddCtrl', function ($scope, ItemsService, CategoryService) {
 
-        $scope.items = [];
-        ItemsService.getItems(function(data) {
-          $scope.items = data;
-        });
-
-        $scope.categories = [];
-        CategoryService.getCategories(function(data) {
-          $scope.categories = data;
-        });
-
-        $scope.showItemSignal = false;
-
-        $scope.addButton = function () {
-
-          $scope.showItemSignal = true;
-        };
-
-        $scope.cancelButton = function () {
-
-          $scope.showItemSignal = false;
-        };
-
-        $scope.deleteCurrentItem = function (item) {
-
-          ItemsService.deleteItem(item.id);
-          ItemsService.getItems(function(data) {
-            $scope.items = data;
-          });
-        };
-
-        $scope.addNewItem = function (item) {
-
-          ItemsService.addItem(item);
-          ItemsService.getItems(function(data) {
-            $scope.items = data;
-          });
-          $scope.showItemSignal = false;
-       };
+    $scope.items = [];
+    ItemsService.getItems(function(data) {
+      $scope.items = data;
     });
+
+    $scope.categories = [];
+    CategoryService.getCategories(function(data) {
+      $scope.categories = data;
+    });
+
+    $scope.showItemSignal = false;
+
+    $scope.addButton = function () {
+
+      $scope.showItemSignal = true;
+    };
+
+    $scope.cancelButton = function () {
+
+      $scope.showItemSignal = false;
+    };
+
+    $scope.deleteCurrentItem = function (item) {
+
+      ItemsService.deleteItem(item.id);
+      ItemsService.getItems(function(data) {
+        $scope.items = data;
+      });
+    };
+
+    $scope.addNewItem = function (item) {
+
+      ItemsService.addItem(item);
+      ItemsService.getItems(function(data) {
+        $scope.items = data;
+      });
+      $scope.showItemSignal = false;
+   };
+});

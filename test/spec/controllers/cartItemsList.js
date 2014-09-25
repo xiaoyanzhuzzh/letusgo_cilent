0dfac5140b18fb232f847787f74f7e4d1a378165
zzh <xiaoyanzhuzzh@sina.com>
2014-09-25 23:12:04 +0800

    rename

--- test/spec/controllers/cartItemsList.js
+++ test/spec/controllers/cartItemsList.js
@@ -85,10 +85,10 @@ describe('CartItemsListCtrl', function () {
 
     beforeEach(function () {
 
-      cartItem = {item: {barcode:'ITEM000000', name: '可口可乐', unit: '瓶', price:3.00, category:'饮品'}, number: 2};
-      spyOn(CartItemOperateService, 'getTotalMoney');
-      spyOn(CartItemOperateService, 'getTotalNumber');
-      spyOn(CartItemOperateService, 'addCartItemNumber');
+      cartItem = {item: {id: 0, barcode:'ITEM000000', name: '可口可乐', unit: '瓶', price:3.00, category:'饮品'}, number: 2};
+      spyOn(CartItemsService, 'getTotalMoney');
+      spyOn(CartItemsService, 'getTotalNumber');
+      spyOn(CartItemsService, 'addCartItemNumber');
       createController();
     });
 

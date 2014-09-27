4011f9e72afa7413b8f67b347043d92ff0206031
zzh <xiaoyanzhuzzh@sina.com>
2014-09-27 08:58:24 +0800

    test post and put function

--- test/spec/service/items.js
+++ test/spec/service/items.js
@@ -45,8 +45,67 @@ describe('ItemsService', function() {
     });
   });
 
-  it ('should have get function', function(){
+  describe('should have putItem function', function() {
 
+    beforeEach(function() {
+
+      items = [ {id: 5,barcode:'ITEM000005', name:'方便面', unit:'袋',price: 4.50, category:'零食'}];
+      $httpBackend.when('PUT', '/api/items' + items[0].id, items[0]).respond(201, 'success');
+    });
+
+    it('that call putItemData function', function() {
+
+      ItemsService.putItem(items[0]);
+
+      $httpBackend.expectPUT('/api/items' + items[0].id).respond(201, 'success');
+      ItemsService.putItem(function() {
+
+        $httpBackend.flush();
+      });
+    });
+  });
+
+  describe('should have addItem function', function() {
+
+    beforeEach(function() {
+
+      items = [ {id: 5,barcode:'ITEM000005', name:'方便面', unit:'袋',price: 4.50, category:'零食'}];
+      $httpBackend.when('POST', '/api/items' + items[0].id, items[0]).respond(201, 'success');
+    });
+
+    it('that call addItemData function', function() {
+
+      ItemsService.addItem(items[0]);
+
+      $httpBackend.expectPOST('/api/items' + items[0].id).respond(201, 'success');
+      ItemsService.addItem(function() {
+
+        $httpBackend.flush();
+      });
+    });
+  });
+
+  describe('should have deleteItem function', function() {
+
+    beforeEach(function() {
+
+      items = [ {id: 5,barcode:'ITEM000005', name:'方便面', unit:'袋',price: 4.50, category:'零食'}];
+      $httpBackend.when('DELETE', '/api/items' + items[0].id).respond(201, 'success');
+    });
+
+    it('that call deleteItemData function', function() {
+
+      ItemsService.deleteItem(items[0].id);
+
+      $httpBackend.expectDELETE('/api/items' + items[0].id).respond(201, 'success');
+      ItemsService.deleteItem(function() {
+
+        $httpBackend.flush();
+      });
+    });
+  });
+
+  it ('should have get function', function(){
 
     spyOn(localStorageService, 'get');
     ItemsService.get('items');

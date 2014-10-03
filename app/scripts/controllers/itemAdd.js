f742e9247f5fb25e03753ef673a818a1528c0c4e
zzh <xiaoyanzhuzzh@sina.com>
2014-10-03 12:17:27 +0800

    rename to itemId

--- app/scripts/controllers/itemAdd.js
+++ app/scripts/controllers/itemAdd.js
@@ -39,7 +39,7 @@ angular.module('letusgoApp')
     $scope.addNewItem = function (item) {
 
       ItemsService.setMaxItemId();
-      ItemsService.getMaxId(function(data) {
+      ItemsService.getMaxItemId(function(data) {
 
         item.id = parseInt(data) + 1;
 
